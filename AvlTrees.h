7a16f2923431bbc3f077167aa5b90de63b36da27
JarodCohen <jarod.cohen@campus.technion.ac.il>
2023-05-16 15:53:52 +0300

    j ai enleve le surplus

--- AvlTrees.h
+++ AvlTrees.h
@@ -21,8 +21,8 @@ typedef enum {
 template <class T>
 class Node {
     int key;
-    int key2;
-    int key3;
+    double key_grade;
+    int key_view;
     T element;
     int height;
     Node* father;
@@ -34,7 +34,10 @@ public:
     ~Node();
     void SetKey(int key);
     int GetKey() const;
-    void SetKey(Node<T>* node);
+    void SetKeyView(int key);
+    int GetKeyView() const;
+    void SetKeyGrade(int key);
+    int GetKeyGrade() const;
     void SetElement(T element_to_set);
     T& GetElement();
     void SetHeight(int new_height);
@@ -46,10 +49,7 @@ public:
     void SetFatherNode(Node* father);
     Node* GetFatherNode();
     int GetBalancedFactor();
-    Node* Get_Following_node();
     int SetNewHeight();
-    Node* BuildAvlSubtree(int height_needed);
-    Node<T>* GetPreviousNode();
 };
 
 template <class T>
@@ -134,7 +134,7 @@ public:
 
 template <class T>
 Node<T>::Node(int key, T element, int height, Node* father, Node* left_son, Node* right_son) :
-        key(key), element(element), height(height), father(father), left_son(left_son), right_son(right_son) {}
+        key(key),key_grade(0), key_view(0), element(element), height(height), father(father), left_son(left_son), right_son(right_son) {}
 
 template <class T>
 Node<T>::~Node()
@@ -155,6 +155,30 @@ int Node<T>::GetKey() const
 }
 
 template <class T>
+void Node<T>::SetKeyGrade(int key_to_set)
+{
+    key_grade = key_to_set;
+}
+
+template <class T>
+int Node<T>::GetKeyGrade() const
+{
+    return key_grade;
+}
+
+template <class T>
+void Node<T>::SetKeyView(int key_to_set)
+{
+    key_view = key_to_set;
+}
+
+template <class T>
+int Node<T>::GetKeyView() const
+{
+    return key_view;
+}
+
+template <class T>
 T& Node<T>::GetElement()
 {
     return element;
@@ -170,11 +194,6 @@ template <class T>
 int Node<T>::GetHeight()
 {
     return height;
-}
-template <class T>
-void Node<T>::SetKey(Node<T>* node)
-{
-    key = node->GetKey();
 }
 
 template <class T>
@@ -234,64 +253,6 @@ int Node<T>::GetBalancedFactor() {
 }
 
 template <class T>
-Node<T>* Node<T>::Get_Following_node(){ //not needed
-    //case1 no father//
-    Node<T>* following_node;
-    if(father== nullptr){
-        if (right_son== nullptr){
-            return nullptr;
-        }
-        following_node=right_son;
-        while (true) {
-            if (following_node->left_son == nullptr) //smallest after him
-                break;
-            following_node = following_node->left_son;
-        }
-        return following_node;
-    }
-    else{    //case2 there is a father
-
-        if ( right_son != nullptr){
-            following_node=right_son;
-            while (true) {
-                if (following_node->left_son == nullptr) //smallest after him
-                    break;
-                following_node = following_node->left_son;
-            }
-            return following_node;
-        }
-        else {
-            //There's no right son but there's a father
-            if (father->left_son == this) //node if a left son
-            {
-                return father;
-            }
-            else if (father->right_son == this) //right son
-            {
-                Node<T> *grandfather = father->father;
-
-                while (true) {
-                    if (grandfather == nullptr) {
-                        return nullptr;
-                    }
-
-                    if (grandfather->left_son == father) {
-                        return grandfather;
-                    }
-
-                    if (grandfather->left_son != father) {
-                        grandfather = grandfather->father;
-                        father = father->father;
-                    }
-                }
-            }
-        }
-    }
-    return nullptr;
-}
-
-
-template <class T>
 int Node<T>::SetNewHeight(){
     if (this == nullptr)
         return -1;
@@ -313,80 +274,6 @@ int Node<T>::SetNewHeight(){
     return 1; //we changed it
 }
 
-
-template <class T>
-Node<T>* Node<T>::BuildAvlSubtree(int height_needed){///??
-
-    if (height_needed <= 0)
-        return nullptr;
-
-    auto* new_node = new Node<T>();
-    new_node->left_son = BuildAvlSubtree(height_needed-1);
-    new_node->right_son = BuildAvlSubtree(height_needed-1);
-    return new_node;
-}
-
-
-
-template <class T>
-Node<T>* Node<T>::GetPreviousNode(){///needed???
-    Node<T> *following;
-    if (father == nullptr) {
-        if (left_son == nullptr) //there's no smallest after him and there's no father
-        {
-            return nullptr;
-        }
-
-        following = left_son; //smallest
-
-        while (true) {
-            if (following->right_son == nullptr) //smallest after him
-                break;
-            following = following->right_son;
-        }
-        return following;
-    }
-
-    else  //there's a father
-    {
-        if (left_son != nullptr) //there's a left son
-        {
-            following = left_son;
-
-            while (true) {
-                if (following->right_son == nullptr) //smallest after him
-                    break;
-                following = following->right_son;
-            }
-            return following;
-        }
-        else {//There's no left son but there's a father
-            if (father->right_son == this) //node is a right son
-            {
-                return father;
-            }
-            else if (father->left_son == this) //left son
-            {
-                Node<T> *grandfather = father->father;
-
-                while (true) {
-                    if (grandfather == nullptr)
-                        return nullptr;
-
-                    if (grandfather->right_son == father) {
-                        return grandfather;
-                    }
-
-                    if (grandfather->right_son != father) {
-                        grandfather = grandfather->father;
-                        father = father->father;
-                    }
-                }
-            }
-        }
-    }
-    return nullptr;
-}
 
 //--------------------------------AVL TREES--------------------------------------------
 
@@ -451,19 +338,9 @@ Node<T>* Avl_tree<T>::FindNodeInGenre(Node<T>* node_to_return){
     {
         return nullptr;
     }
-    Node<T>* current_node = root;
-    while (node_to_return->GetKey()!=current_node->GetKey() && current_node != nullptr){
-        if(node_to_return->GetElement < current_node->GetElement){
-            node_to_return=node_to_return->GetNodeLeft();
-            current_key=node_to_return->GetKey();
-            else {
-                node_to_return=node_to_return->GetNodeRight();
-                current_key=node_to_return->GetKey();
-            }
-        }
-        return node_to_return;
-    }
-};
+    
+    return node_to_return;
+}
 
 template <class T>
 AVLResults Avl_tree<T>::InsertNode(Node<T>* node){
@@ -504,292 +381,11 @@ AVLResults Avl_tree<T>::InsertNode(Node<T>* node){
 
 template <class T>
 AVLResults Avl_tree<T>::InsertNodeInGenre(Node<T>* node){
-
-}
-
-
-template <class T>
-AVLResults Avl_tree<T>::InsertNodeInGenre(int key, int rate , int view){
-}
-
-
-template <class T>
-AVLResults Avl_tree<T>::RemoveNode(int key_to_remove) {
-
-    if (key_to_remove<0)
-        return AVL_NULL_ARGUMENT;
-
-    Node<T>* node_to_remove= FindNode(root, key_to_remove);
-    if (node_to_remove== nullptr)
-        return AVL_NODE_NO_EXIST;
-
-    if (node_to_remove->GetNodeRight()== nullptr && node_to_remove->GetNodeLeft()== nullptr)
-        return this->Remove_Node_Leaf(node_to_remove);
-
-    else if (node_to_remove->GetNodeRight()== nullptr || node_to_remove->GetNodeLeft()== nullptr)
-        return this->Remove_Node_With_Only_Child(node_to_remove);
-
-    //The node has 2 children, we look for the "following" node and we swap them
-    //then we delete the node to remove
-    Node<T>* following_node= node_to_remove->Get_Following_node();
-    /*there's a following node because if there's not it means the node_to_remove is the biggest
-    node and that means it's the leaf at the right of the tree */
-    Swap(node_to_remove, following_node);
-    if(node_to_remove->GetFatherNode()== nullptr)
-    {
-        this->root=following_node;
-        following_node->SetFatherNode(nullptr);
-    }
-
-    else if(node_to_remove->GetFatherNode()!= nullptr)
-    {
-        if(node_to_remove->GetNodeRight()== nullptr) {
-            if (node_to_remove->GetFatherNode()->GetNodeRight() == node_to_remove)
-                node_to_remove->GetFatherNode()->SetNodeRight(nullptr);
-            else if (node_to_remove->GetFatherNode()->GetNodeLeft() == node_to_remove)
-                node_to_remove->GetFatherNode()->SetNodeLeft(nullptr);
-        }
-
-        else if(node_to_remove->GetNodeRight() != nullptr)
-        {
-            node_to_remove->GetNodeRight()->SetFatherNode(node_to_remove->GetFatherNode());
-            if (node_to_remove->GetFatherNode()->GetNodeRight() == node_to_remove)
-                node_to_remove->GetFatherNode()->SetNodeRight(node_to_remove->GetNodeRight());
-            else if (node_to_remove->GetFatherNode()->GetNodeLeft() == node_to_remove)
-                node_to_remove->GetFatherNode()->SetNodeLeft(node_to_remove->GetNodeRight());
-        }
-    }
-
-    //fix the heights of the fathers
-    Node<T>* father = node_to_remove->GetFatherNode();
-    Node<T>* temp =father;
-    while(temp!= nullptr)
-    {
-        if(temp->SetNewHeight()==0)
-            break;
-        temp= temp->GetFatherNode();
-    }
-
-    //We now check the BF of all the nodes from here to the root
-    if(father == nullptr)
-    {
-        return AVL_SUCCESS;
-    }
-    int BF = father->GetBF();
-
-    while (true)
-    {
-        if (BF==2){
-            if(father->GetNodeLeft() != nullptr) {
-
-                if ((father->GetNodeLeft())->GetBF() == -1)
-                    RotateLR(father);
-                else if ((father->GetNodeLeft())->GetBF() >= 0)
-                    RotateLL(father);
-            }
-        }
-
-        if (BF==-2){
-            if(father->GetNodeRight() != nullptr) {
-                if ((father->GetNodeRight())->GetBF() == 1)
-                    RotateRL(father);
-                else if ((father->GetNodeRight())->GetBF() <= 0)
-                    RotateRR(father);
-            }
-        }
-
-        if (father->GetFatherNode()== nullptr)
-            break;
-        father=father->GetFatherNode();
-        BF = father->GetBF();
-    }
-
-    delete node_to_remove;
     return AVL_SUCCESS;
 }
 
-//if it's a leaf, remove it and then check the BF of every node before and fix it.
+
 template <class T>
-AVLResults Avl_tree<T>::Remove_Node_Leaf(Node<T>* node) {
-    if (node == nullptr)
-        return AVL_NULL_ARGUMENT;
-
-    //Remove the node
-    Node<T>* father = node->GetFatherNode();
-
-    if(father== nullptr) //the node is the root and has no son
-    {
-        this->root = nullptr;
-        delete node;
-        return AVL_SUCCESS;
-    }
-
-    //the node is a left leaf
-    if (father->GetNodeLeft()==node) {
-        father->SetNodeLeft(nullptr);
-        if(father->GetNodeRight()== nullptr) {
-            father->SetHeight(0);
-            Node<T>* temp= father;
-            while(temp->GetFatherNode()!= nullptr)
-            {
-                //if the node didnt change the height, we stop checking further
-                if (temp->GetFatherNode()->SetNewHeight()==0)
-                    break;
-                temp= temp->GetFatherNode();
-            }
-        }
-    }
-
-    //the node is a right leaf
-    if (father->GetNodeRight()==node) {
-        father->SetNodeRight(nullptr);
-
-        if (father->GetNodeLeft() == nullptr) { //change the height
-            father->SetHeight(0);
-            Node<T> *temp = father;
-            while (temp->GetFatherNode() != nullptr) {
-                //if the node didnt change the height, we stop checking further
-                if (temp->GetFatherNode()->SetNewHeight() == 0)
-                    break;
-                temp = temp->GetFatherNode();
-            }
-        }
-    }
-
-    delete node;
-
-    //then check the BF of every node from father to the root.
-    int BF = father->GetBF();
-
-    while (true)
-    {
-        if (BF==2){
-            if(father->GetNodeLeft() != nullptr) {
-                if ((father->GetNodeLeft()->GetBF() == -1))
-                    RotateLR(father);
-                else if ((father->GetNodeLeft())->GetBF() >= 0)
-                    RotateLL(father);
-            }
-        }
-
-        if (BF==-2){
-            if(father->GetNodeRight() != nullptr) {
-                if ((father->GetNodeRight())->GetBF() == 1)
-                    RotateRL(father);
-                else if ((father->GetNodeRight())->GetBF() <= 0)
-                    RotateRR(father);
-            }
-        }
-
-        if (father->GetFatherNode()== nullptr)
-            break;
-        father=father->GetFatherNode();
-        BF = father->GetBF();
-    }
-
-    return AVL_SUCCESS;
-}
-
-//if there's one son, remove it and replace it in the "father" node with his son
-// and then check the BF of every node before and fix it.
-template <class T>
-AVLResults Avl_tree<T>::Remove_Node_With_Only_Child(Node<T>* node) {
-    if (node== nullptr)
-        return AVL_NULL_ARGUMENT;
-
-    Node<T>* father = node->GetFatherNode();
-    if(father == nullptr) //node is root and has only one son
-    {
-        if (node->GetNodeRight() == nullptr)
-        {
-            this->root = node->GetNodeLeft();
-            node->GetNodeLeft()->SetFatherNode(nullptr);
-            delete node;
-            return AVL_SUCCESS;
-        }
-
-        if (node->GetNodeLeft() == nullptr)
-        {
-            this->root = node->GetNodeRight();
-            node->GetNodeRight()->SetFatherNode(nullptr);
-            delete node;
-            return AVL_SUCCESS;
-        }
-    }
-
-    //first remove the node and replace it with his son.
-    if(father->GetNodeLeft()==node) {
-        if (node->GetNodeRight() == nullptr) {
-            father->SetNodeLeft(node->GetNodeLeft());
-            node->GetNodeLeft()->SetFatherNode(father);
-        }
-        else {
-            father->SetNodeLeft(node->GetNodeRight());
-            node->GetNodeRight()->SetFatherNode(father);
-        }
-    }
-
-    else {
-        if (node->GetNodeRight() == nullptr) {
-            father->SetNodeRight(node->GetNodeLeft());
-            node->GetNodeLeft()->SetFatherNode(father);
-        }
-        else {
-            father->SetNodeRight(node->GetNodeRight());
-            node->GetNodeRight()->SetFatherNode(father);
-        }
-    }
-
-    //fix the heights of the fathers
-    Node<T>* temp =father;
-    while(temp!= nullptr)
-    {
-        if(temp->SetNewHeight()==0)
-            break;
-        temp= temp->GetFatherNode();
-    }
-
-    delete node;
-
-    //then check the BF and fix it from father up to the root
-    if(father == nullptr)
-    {
-        return AVL_SUCCESS;
-    }
-    int BF = father->GetBF();
-
-    while (true)
-    {
-        if (BF==2){
-            if(father->GetNodeLeft() != nullptr) {
-
-                if ((father->GetNodeLeft())->GetBF() == -1)
-                    RotateLR(father);
-                else if ((father->GetNodeLeft())->GetBF() >= 0)
-                    RotateLL(father);
-            }
-        }
-
-        if (BF==-2){
-            if(father->GetNodeRight() != nullptr) {
-
-                if ((father->GetNodeRight())->GetBF() == 1)
-                    RotateRL(father);
-                else if ((father->GetNodeRight())->GetBF() <= 0)
-                    RotateRR(father);
-            }
-        }
-
-        if (father->GetFatherNode()== nullptr)
-            break;
-        father=father->GetFatherNode();
-        BF = father->GetBF();
-    }
-
-    return AVL_SUCCESS;
-}
-
-template <class T> // not needed
 void Avl_tree<T>::Swap(Node<T>* node_1, Node<T>* node_2){
     int temp_height = node_1->GetHeight;
     Node<T>* temp_left_son = node_1->GetNodeLeft();
